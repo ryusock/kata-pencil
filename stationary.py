1c10b1820077ca5471179af5f434775e044b7a0c
Seokhyeon Ryu <seokhyeonryu87@gmail.com>
2018-11-08 14:18:20 -0500

    Add sharpening feature, pass more complex writing tests

--- stationary.py
+++ stationary.py
@@ -1,11 +1,34 @@
 class Pencil:
-    def __init__(self, point_dur=10, eraser_dur=10, length=2):
+    def __init__(self, point_dur=10, length=5, eraser_dur=10):
         self.point_dur = point_dur
+        self.default_point_dur = point_dur
         self.eraser_dur = eraser_dur
         self.length = length
 
     def write(self, text, paper):
-        paper.append(text)
+        if not isinstance(text, str):
+            raise TypeError('Please provide a string argument')
+
+        writeable = ""
+        for c in text:
+            cost = 1
+            if c == ' ' or c == '\n':
+                cost = 0
+            if c.isupper():
+                cost = 2
+            # print(c, cost, self.point_dur)
+            if self.point_dur - cost >= 0:
+                writeable += c
+            elif self.length > 0:
+                self.length -= 1
+                self.point_dur += self.default_point_dur
+                writeable += c
+            self.point_dur -= cost
+
+        paper.append(writeable)
+
+    # def sharpen(self):
+
 
 class Paper:
     def __init__(self):
