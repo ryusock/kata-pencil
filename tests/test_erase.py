b84c90649924b8d271b2318b032bcaeb380fb4bf
Seokhyeon Ryu <seokhyeonryu87@gmail.com>
2018-11-09 09:43:52 -0500

    Refactor testing functions

--- tests/test_erase.py
+++ tests/test_erase.py
@@ -3,3 +3,20 @@ import sys
 sys.path.append('../src')
 
 from stationary import Pencil, Paper
+
+@pytest.fixture
+def paper():
+    '''Returns a black sheet of Paper'''
+    return Paper()
+
+@pytest.fixture
+def pencil():
+    '''Returns a Pencil with default parameters'''
+    return Pencil()
+
+def test_erase_raises_exception_on_nonstring_argument(pencil, paper):
+    with pytest.raises(TypeError):
+        pencil.erase(100, paper)
+
+def test_erase_nothing():
+    return
--- tests/test_write.py
+++ tests/test_write.py
@@ -4,46 +4,45 @@ sys.path.append('../src')
 
 from stationary import Pencil, Paper
 
-def test_write_raises_exception_on_nonstring_argument():
-    pencil = Pencil()
-    paper = Paper()
+@pytest.fixture
+def paper():
+    '''Returns a black sheet of Paper'''
+    return Paper()
+
+@pytest.fixture
+def pencil():
+    '''Returns a Pencil with default parameters'''
+    return Pencil()
+
+def test_write_raises_exception_on_nonstring_argument(pencil, paper):
     with pytest.raises(TypeError):
         pencil.write(100, paper)
 
-
-def test_write_initial():
-    pencil = Pencil()
-    paper = Paper()
+def test_write_initial(pencil, paper):
     pencil.write("She sells sea shells", paper)
     assert paper.text == "She sells sea shells"
 
-def test_write_append():
-    pencil = Pencil()
-    paper = Paper()
+def test_write_append(pencil, paper):
     pencil.write("She sells sea shells", paper)
     pencil.write(" down by the sea shore", paper)
     assert paper.text == "She sells sea shells down by the sea shore"
 
-def test_write_point_degradation_no_caps():
+def test_write_point_degradation_no_caps(paper):
     pencil = Pencil(4, 0)
-    paper = Paper()
     pencil.write("sudoku", paper)
     assert paper.text == "sudo"
 
-def test_write_point_degradation_caps():
+def test_write_point_degradation_caps(paper):
     pencil = Pencil(4, 0)
-    paper = Paper()
     pencil.write("Sudoku", paper)
     assert paper.text == "Sud"
 
-def test_write_point_degradation_at_cap():
+def test_write_point_degradation_at_cap(paper):
     pencil = Pencil(4, 0)
-    paper = Paper()
     pencil.write("sudO", paper)
     assert paper.text == "sud"
 
-def test_write_point_degradation_with_blanks():
+def test_write_point_degradation_with_blanks(paper):
     pencil = Pencil(10, 0)
-    paper = Paper()
     pencil.write("sudoku\n\n   ch a\nll\nenge", paper)
     assert paper.text == "sudoku\n\n   ch a\nl"
